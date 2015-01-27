1a8091d7e4041c0ee39e904e0a588ac6e69878d3
Jeff Cohen <cohen.jeff@gmail.com>
2015-01-27 12:26:26 -0600

    Homework for Week 4

---
+++ README.md
@@ -0,0 +1,20 @@
+MPCS 50101: Homework for Week 4
+===============================
+
+There is one challenge this week.
+
+### Online Banking Simulation
+
+Open the `bank_accounts.py` file.
+
+Your job is to make the code run without any errors.  If you're successful, you should see the following when you run the program:
+
+```
+This bank seems trustworthy!  Here are the final balances
+Checking account:  1500.0
+Savings account:  500.0
+```
+
+Good luck and have fun!  Post a message to Piazza if you have any questions.
+
+
---
+++ bank_accounts.py
@@ -0,0 +1,45 @@
+# Can you make this code work?
+
+import banking
+from sys import exit
+
+my_checking_account = banking.BankAccount()
+my_savings_account = banking.BankAccount()
+
+# 1. Basic account functionality
+my_checking_account.balance = 0.00
+my_checking_account.deposit(300.00)
+my_checking_account.deposit(300.00)
+my_checking_account.deposit(300.00)
+my_checking_account.deposit(300.00)
+my_checking_account.withdraw(200.00)
+
+if my_checking_account.balance != 1000.00:
+    print("This bank can't be trusted! Failed on step 1.")
+    exit()
+
+# 2. Transfer between two accounts
+my_savings_account.balance = 1000.00
+my_savings_account.transfer(500.00, my_checking_account)
+
+if my_checking_account.balance != 1500.00:
+    print("This bank can't be trusted! Failed at step 2.")
+    exit()
+
+# 3. Guard against illegal activities
+my_checking_account.withdraw(10000.00)
+if my_checking_account.balance != 1500.00:
+    print("This bank can't be trusted! Failed at step 3.")
+
+my_checking_account.transfer(10000.00, my_savings_account)
+if my_checking_account.balance != 1500.00:
+    print("This bank can't be trusted! Failed at step 3.")
+
+# At this point the checking account should have $1500
+# and the savings account should have $500
+print("This bank seems trustworthy!  Here are the final balances:")
+print("Checking account: ", my_checking_account.balance)
+print("Savings account: ", my_savings_account.balance)
+
+
+
